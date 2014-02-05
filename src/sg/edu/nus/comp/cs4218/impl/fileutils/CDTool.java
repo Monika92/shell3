0f764326ddcb82591e03e5f6240b25f5570b6a8c
madhumaithri <madhumaithri@gmail.com>
2014-02-06 04:09:17 +0800

    Madhu : cd more changes and bug fixes

--- src/sg/edu/nus/comp/cs4218/impl/fileutils/CDTool.java
+++ src/sg/edu/nus/comp/cs4218/impl/fileutils/CDTool.java
@@ -1,6 +1,7 @@
 package sg.edu.nus.comp.cs4218.impl.fileutils;
 
 import java.io.File;
+
 import sg.edu.nus.comp.cs4218.impl.ATool;
 import sg.edu.nus.comp.cs4218.impl.WorkingDirectory;
 import sg.edu.nus.comp.cs4218.fileutils.ICdTool;
@@ -19,26 +20,67 @@ public class CDTool extends ATool implements ICdTool{
 		return null;	
 	}
 	
+	/*
+	private static boolean testPath(String path) {
+	    int prefixLen = (new File(path)).getPrefixLength();
+	    if (testPathWin(path, prefixLen) || testPathLinux(prefixLen))
+	        return true;
+	    else
+	        return false;
+	}
+
+	private static boolean testPathWin(String path, int prefixLen) {
+	    if (prefixLen == 3)
+	        return true;
+	    File f = new File(path);
+	    if ((prefixLen == 2) && (f.getPath().charAt(0) == '/'))
+	        return true;
+	    return false;
+	}
+
+	private static boolean testPathLinux(int prefixLen) {
+	    return (prefixLen != 0);
+	}
+	*/
+	public Boolean isAbsolutePath(String dir)
+	{
+		if (System.getProperty("os.name").startsWith("Windows")) 
+		{
+	        
+	    } 
+		else 
+	    {
+	        if(dir.indexOf('/')==0)
+	        	return true;
+	        else
+	        	return false;
+	    } 
+		return null;
+	}
+	
 	public File getDirectoryPath(String dirName , String workingDir)
 	{
-		if((dirName.indexOf('\\')==0) || (dirName.contentEquals(".")))
-		{
-			return (new File(dirName));
-		}
-		else if((dirName.indexOf('~')==0))
-		{
-			dirName = dirName.replaceFirst("~", workingDir + '\\');
-			return (new File(dirName));
-		}
-		else if(dirName.contentEquals(".."))
+		
+		if(dirName.contentEquals(".."))
 		{
 			File wd = new File(workingDir);
-			dirName = wd.getParentFile().getAbsolutePath();
+			if(wd.getParentFile()!=null)
+			dirName = wd.getParentFile().getAbsolutePath();	
+			return (new File(dirName));
+		}
+		else if(dirName.contentEquals("."))
+		{
+			//remain as it is
+			File wd = new File(workingDir);
+			dirName = wd.getAbsolutePath();
 			return (new File(dirName));
 		}
 		else
 		{
-			return (new File(workingDir + '\\' + dirName));
+			//absolute and relative paths
+			File dir = new File(dirName);
+			dirName = dir.getAbsolutePath();
+			return (new File(dirName));
 		}
 		
 	}
@@ -62,6 +104,12 @@ public class CDTool extends ATool implements ICdTool{
 		{
 			//To move to a directory using a full pathname: ex.   cd /home/physics/ercy04/ProjectX
 			//File dir = new File(args[0]);
+			if(args[0].contains("~"))
+			{			
+				String home = System.getProperty("user.home") + File.separator;
+				args[0] = args[0].replace("~", home);
+			}
+			
 			File dir = getDirectoryPath(args[0] , workingDir.getAbsolutePath());
 			if(dir.isDirectory()) 
 	        {
