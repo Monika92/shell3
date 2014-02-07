0e87b463dcab3536f4ff161061b42bb5cb60d87b
dchrislene <a0075133@nus.edu.sg>
2014-02-07 23:34:06 +0800

    Fixes COMM

--- test/sg/edu/nus/comp/cs4218/impl/extended2/COMMToolTest.java
+++ test/sg/edu/nus/comp/cs4218/impl/extended2/COMMToolTest.java
@@ -120,7 +120,7 @@ public class COMMToolTest {
 		commTool = new COMMTool(arguments);		
 		actualOutput = commTool.execute(workingDirectory, null);
 
-		expectedOutput = null;
+		expectedOutput = "File 1 doesn't exist!";
 		assertEquals(expectedOutput, actualOutput);	
 
 	}
@@ -217,6 +217,21 @@ public class COMMToolTest {
 				testDash + testTab + "Banana" + testTab +testDash + testNewline +
 				testDash + testTab + testDash + testTab + "Melon" + testNewline +
 				testDash + testTab + testDash + testTab + "Orange";
+
+		assertTrue(expectedOutput.equalsIgnoreCase(actualOutput));
+		assertEquals(commTool.getStatusCode(), 0);	
+	}
+	
+
+	@Test
+	//Negative case: File 2 not sorted
+	public void compareFilesCheckSortStatusTest2(){
+		String[] arguments = new String[]{"-c","a.txt","d.txt"};
+		String input1 = "a.txt";
+		String input2 = "d.txt";
+		commTool = new COMMTool(arguments);
+		actualOutput = commTool.compareFilesCheckSortStatus(input1, input2);
+		expectedOutput = "File 2 not sorted!\n";
 
 		assertTrue(expectedOutput.equalsIgnoreCase(actualOutput));
 		assertEquals(commTool.getStatusCode(), 0);	
